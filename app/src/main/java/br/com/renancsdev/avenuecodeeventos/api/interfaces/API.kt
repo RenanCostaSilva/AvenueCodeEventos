a15b5f7fbbf156f2d76aa0c2143ed584b483babb
Renan Costa e Silva <renancsdev@gmail.com>
2023-10-03 18:12:47 -0300

    Update 1.4
    Adicionado:
    validação de email
    componente include
    melhoria de layout
    
    Removido:
    Warnings

--- app/src/main/java/br/com/renancsdev/avenuecodeeventos/api/interfaces/API.kt
+++ app/src/main/java/br/com/renancsdev/avenuecodeeventos/api/interfaces/API.kt
@@ -1,8 +1,6 @@
 package br.com.renancsdev.avenuecodeeventos.api.interfaces
 
 import br.com.renancsdev.avenuecodeeventos.model.Evento
-import br.com.renancsdev.avenuecodeeventos.model.Resultados
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
@@ -11,20 +9,6 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface API {
-    
-    @GET("events/{id}")
-    fun buscarEventoPorID(@Path("id") id: Int): Call<Evento>
-
-    @GET("events")
-    fun buscarEventos(): Call<ArrayList<Evento>>
-
-    @FormUrlEncoded
-    @POST("checkin")
-    fun fazerCheckIn(@Field("name") nome: String , @Field("email") email: String ,  @Field("eventId") id: Int): Call<Evento>
-
-
-
-    // Live Data
 
     @GET("events/{id}")
     suspend fun buscarEventoPorIDLiveData(@Path("id") id: Int): Response<Evento>
